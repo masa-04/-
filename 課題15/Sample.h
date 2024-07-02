a72e27f25e26944a27a5b77ac3bc0cea28b80435
masa1250 <230425@jc-21.jp>
2024-07-02 12:59:22 +0900

    課題15まで

---
+++ 課題15/Sample.h
@@ -0,0 +1,17 @@
+/*
+Sample.h
+SampleClass クラスを宣言
+*/
+//クラスを宣言
+class SampleClass {
+public:
+	//メンバ変数
+	int a;
+	int b;
+	int c;
+	//メンバ関数
+public:
+	void Input();
+	void Plus();
+	void Disp();
+};
---
+++ 課題15/Sub.h
@@ -0,0 +1,8 @@
+#include "sample.h"
+//サンプルクラスを継承したクラスを宣言
+class Sub : public SampleClass
+{
+	//メンバ関数
+public:
+	void Minus();
+};
